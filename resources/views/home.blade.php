12a2e65290f2fcda5f4fff9cf1a7a0f6e90ab3ce
Ilham Pradipta Listiyanto <55991471+AMTATO@users.noreply.github.com>
2023-10-05 17:07:29 +0700

    v1.0.0 - Create hero section part 1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>
-    Initialize Project
-</h1>
+    <section id="hero" class="hero w-100 h-100 p-3 mx-auto d-flex justify-content-center align-items-center text-center text-white">
+        <main>
+            <h4>Kepada Bapak/Ibu/Saudara/i,</h4>
+            <h1>Tanto & Tanti</h1>
+            <p>Akan melangsungkan resepsi pernikahan dalam:</p>
+            <a href="#undangan" class="btn btn-lg mt-4">Lihat Undangan</a>
+        </main>
+    </section>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="lang={{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,11 +8,18 @@
     <title>Wedding Invitation</title>
 
     @vite('resources/sass/app.scss')
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sacramento&family=Work+Sans:wght@100;400;600;700&display=swap"
+        rel="stylesheet">
+    @vite('resources/css/app.css')
 </head>
+
 <body>
     @yield('content')
 
     <!-- Core plugin JavaScript-->
     @vite('resources/js/app.js')
 </body>
+
 </html>
