53fef6c6831f85b56ae071918fb62e6e3224e9bb
Ilham Pradipta Listiyanto <55991471+AMTATO@users.noreply.github.com>
2023-10-07 22:47:19 +0700

    v1.0.0 - Create hero section part 2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
-import './bootstrap';
+import "./bootstrap";
+import "./simplyCountdown.min.js";
+
+// Simply Countdown Start
+simplyCountdown(".simply-countdown", {
+    year: 2023, // required
+    month: 11, // required
+    day: 25, // required
+    words: {
+        //words displayed into the countdown
+        days: { singular: "hari", plural: "hari" },
+        hours: { singular: "jam", plural: "jam" },
+        minutes: { singular: "menit", plural: "menit" },
+        seconds: { singular: "detik", plural: "detik" },
+    },
+});
+// Simply Countdown End
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
             <h4>Kepada Bapak/Ibu/Saudara/i,</h4>
             <h1>Tanto & Tanti</h1>
             <p>Akan melangsungkan resepsi pernikahan dalam:</p>
+            <div class="simply-countdown"></div>
             <a href="#undangan" class="btn btn-lg mt-4">Lihat Undangan</a>
         </main>
     </section>
