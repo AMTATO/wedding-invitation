80bdac9ce5f4fcb5be9bf1c92239c79fa577f026
Ilham Pradipta Listiyanto <55991471+AMTATO@users.noreply.github.com>
2023-10-10 22:20:40 +0700

    v1.2.0 - Create navigation

---
+++ resources/views/layouts/navigation.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-md bg-body-tertiary stiky-top mynavbar">
+    <div class="container">
+        <a class="navbar-brand fw-bold" href="#">Wedding Invitation</a>
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
+            aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title fw-bold" id="offcanvasNavbarLabel">Wedding Invitation</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <div class="navbar-nav ms-auto text-uppercase">
+                    <a class="nav-link" href="#home">Home</a>
+                    <a class="nav-link" href="#info">Info</a>
+                    <a class="nav-link" href="#story">Story</a>
+                    <a class="nav-link" href="#gallery">Gallery</a>
+                    <a class="nav-link" href="#rsvp">RSVP</a>
+                    <a class="nav-link" href="#gifts">Gifts</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
